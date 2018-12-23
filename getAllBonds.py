5714e7c5a1b932a06f7baa190a488abe1ab9852e
Igor Gorbenko <igor_gorbenko@list.ru>
2018-12-23 23:41:28 +0300

    Fix: checking the type of 'page' parameter has been added

--- getAllBonds.py
+++ getAllBonds.py
@@ -1,7 +1,6 @@
 import json
 import boto3
 
-
 def lambda_handler(event, context):
 
     s3 = boto3.resource('s3')
@@ -13,24 +12,23 @@ def lambda_handler(event, context):
     response_body = []
 
     collection_per_page = 10
-    page_count = -(-len(lines) // collection_per_page)
+    page_count = -(-len(lines) // collection_per_page) - 1
 
     """ Mandatory parameter is the page """
     if 'page' in event:
-        num_page = event['page']
+        if event['page'].isdigit():
+            num_page = int(event['page'])
+        else:
+            return {
+                'statusCode': 403,
+                'page' : -1,
+                'body': "Please, check the parameter 'Number of a page' at the query. It should be an integer"
+            }
     else:
         return {
             'statusCode': 403,
             'page' : -1,
             'body': "Mandatory parameter 'Page' is missing at the query"
-        }
-
-    """ Number of page should be integer """
-    if not isinstance(num_page, int):
-        return {
-            'statusCode': 403,
-            'page' : -1,
-            'body': "Please, check the parameter 'Number of a page' at the query"
         }
 
     if num_page > page_count:
@@ -63,12 +61,9 @@ def lambda_handler(event, context):
         return {
             'statusCode': 400,
             'page' : -1,
-            'body': 'It seems an ERROR!' + e
+            'body': 'It seems an ERROR!' + str(e)
         }
 
 def pagination(seq, rows_per_page):
     for start in range(1, len(seq), rows_per_page):
         yield seq[start:start + rows_per_page]
-
-if __name__ == '__main__':
-    lambda_handler({"page": 0}, '')
